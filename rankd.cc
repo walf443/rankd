58e8515ffb4229e07ebf80216607f2e500c0dfe7
Keiji, Yoshimi <walf443@gmail.com>
2012-02-09 08:56:22 +0900

    既存のものをupdateする処理をやめてみたらまともになったっぽい

--- rankd.cc
+++ rankd.cc
@@ -48,6 +48,7 @@ namespace rankd {
         }
     }
 
+    // TODO: どこのデータからupdateされたのか、を考慮してより効率よく更新する
     void Manager::update_rank_map(Node* node)
     {
         Node* nd = this->first;
@@ -55,37 +56,62 @@ namespace rankd {
         unsigned long n = 1;
         while ( n < this->num_of_items ) {
             std::map<unsigned long, Node*>::iterator iter = this->rank_map.find(n);
-            if ( iter == this->rank_map.end() ) {
+            // if ( iter == this->rank_map.end() ) {
+                // rank_mapにデータがない場合なので、基本的にはそれ以上上位のランキングは存在するはずなので、
+                // 最も近いデータから順番にたどる
                 do {
                     if ( n == rank ) {
+                        std::cout << "updated_rank_map rank: " << n << std::endl;
+                        std::cout << "updated_rank_map nd: " << nd << std::endl;
+                        std::cout << "updated_rank_map item_id: " << nd->value << std::endl;
                         break;
                     }
 
                     rank++;
                 } while ( nd = nd->next );
                 this->rank_map.insert(std::make_pair(n, nd));
-            } else {
-                // topから呼ばれているのを想定している
-                //
-                // データをひとつズラす
-                this->rank_map.insert(std::make_pair(n, iter->second->prev));
-            }
+            // } else {
+            //     // topから呼ばれているのを想定している
+            //     //
+            //     // データをひとつズラす
+            //     std::cout << "updated_rank_map already rank: " << n << std::endl;
+            //     Node* nd = (Node *)iter->second->prev;
+            //     std::cout << "updated_rank_map already nd: " << nd << std::endl;
+            //     std::cout << "updated_rank_map already item_id: " << nd->value << std::endl;
+            //     this->rank_map.insert(std::make_pair(n, iter->second->prev));
+            // }
             n *= 2;
         }
     }
 
     unsigned long Manager::get_rank(unsigned long item_id)
     {
-        unsigned long rank = 1;
-        Node* node = this->first;
+        std::map<Node*, unsigned long> reverse_rank_map;
+        std::map<unsigned long, Node*>::iterator iter = this->rank_map.begin();
+        while ( iter != this->rank_map.end() ) {
+            reverse_rank_map.insert(std::make_pair(iter->second, iter->first));
+
+            iter++;
+        }
+
+        Node* node;
+        std::map<unsigned long, Node*>::iterator item_iter = this->item_map.find(item_id);
+        if ( item_iter == this->item_map.end() ) {
+            return 0;
+        }
+        node = item_iter->second;
+
+        unsigned long counter = 0;
         do {
-            if ( node->value == item_id ) {
-                return rank;
+            std::map<Node*, unsigned long>::iterator rank_iter = reverse_rank_map.find(node);
+            if ( rank_iter != reverse_rank_map.end() ) {
+                return rank_iter->second + counter;
             }
 
-            rank++;
-        } while ( node = node->next );
-        return 0;
+            counter++;
+        } while ( node = node->prev );
+        return 1;
+
     }
 
     Node* Manager::get_node_by_rank(unsigned long rank)
@@ -103,15 +129,18 @@ namespace rankd {
 
 #include <iostream>
 
+#define NUM 1000
+
 int main (int argc, char **argv)
 {
     rankd::Manager* manager = new rankd::Manager();
-    for (int i = 0; i < 1000000; i++ ) {
+    for (int i = 0; i < NUM; i++ ) {
         manager->top(i);
     }
     for (int i = 0; i < 100; i++ ) {
-        std::cout << i << ": " << manager->get_rank(i) << std::endl;
+        // std::cout << i << ": " << manager->get_rank(i) << std::endl;
     }
+
     delete manager;
 
     return 0;
